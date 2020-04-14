a446753d078cb3f3c5f81e4c2406b48d6ad23993
Fabio Gadau <fabiogadau92@gmail.com>
2020-04-14 14:37:35 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,37 @@
+$(document).ready( function() {
+  /*
+  * Ricreare la navbar di revolut
+    Rendere funzionali i menu dropdown all’interno della navigazione principale
+    Importante l’uso di $(this)
+    Usare il click() oppure mouseenter() / mouseleave() per mostrare / nascondere i dropdown menu
+    Modificare l’icona freccia nel selettore della lingua all’hover (freccia giù / freccia su) usando fontawesome
+  */
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
