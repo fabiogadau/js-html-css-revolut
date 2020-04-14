be2bc7e2acdf3f8a8eee0869d8ae93cdf3cac2bc
Fabio Gadau <fabiogadau92@gmail.com>
2020-04-14 16:54:01 +0200

    almost completed

--- script/main.js
+++ script/main.js
@@ -6,8 +6,24 @@ $(document).ready( function() {
     Usare il click() oppure mouseenter() / mouseleave() per mostrare / nascondere i dropdown menu
     Modificare l’icona freccia nel selettore della lingua all’hover (freccia giù / freccia su) usando fontawesome
   */
-  
 
+  // Referenze
+  var dropDown = $('.top-actions-list .with-dropdown');
+  var dropLinks = dropDown.children('a');
+  var dropdownMenu = dropDown.children('.dropdown-menu');
+
+  // Visualizzo/Nascondo dropdown menu
+  dropLinks.click(function(event) {
+
+    event.preventDefault();
+
+    var actualMenu = $(this).next('.dropdown-menu');
+
+    dropdownMenu.not(actualMenu).hide();
+
+    actualMenu.toggle();
+
+  });
 
 
 
